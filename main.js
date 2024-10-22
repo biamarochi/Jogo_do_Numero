00bb15e22c18e0d27e5b71f72c0a81423d992558
biamarochi <163007667+biamarochi@users.noreply.github.com>
2024-10-22 08:49:11 -0300

    Update main.js

--- main.js
+++ main.js
@@ -35,10 +35,37 @@ baixoOuAlto.textContent = "O último palpite foi muito baixo";
     }
 }
 
+    contagemPalpites++;
+    campoPalpite.value = "";
+    campoPalpite.focus();
+}
+
+envioPalpite.addEventListener('click', verificarPalpite);
+
+function finalizarJogo() {
+    campoPalpite.disabled = true;
+    envioPalpite.disabled = true;
+    botaoReiniciar = document.createElement('button');
+    document.body.appendCHilld(botaoReiniciar);
+    botaoReiniciar.addEventListener('click', reiniciarJogo);
 }
 
 
+function reiniciarJogo() {
+    contagemPalpites = 1;
+    const paragrafosReiniciar = document.querySelectorAll('.paragrafosResultado p');
+    for (const paragrafoReiniciar of paragrafosReiniciar) {
+        paragrafoReiniciar.textContent = "";
+    }
 
+    botaoReiniciar.parentNode.removeChild(botaoReiniciar);
+    campoPalpite.disabled = 'false';
+    envioPalpite.disabled = 'false';
+    campoPalpite.value = "";
+    campoPalpite.focus();
+    ultimoResultado.style.backgroundColor = 'white';
+    numeroAleatorio = Math.floor(Math.random() * 100) + 1;
+}
 
 
 
